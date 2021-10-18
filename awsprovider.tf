6c01471e1e18abb629c9ce04d11581c995e6c96e
RAMKUMAR-devops <59164612+RAMKUMAR-devops@users.noreply.github.com>
2021-10-18 08:25:01 -0500

    Update awsprovider.tf

--- awsprovider.tf
+++ awsprovider.tf
@@ -11,3 +11,27 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "terraformbackend"
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraformbackend"
+    key = "terraform"
+    region = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
