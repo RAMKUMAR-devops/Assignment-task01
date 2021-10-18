164fde4a84869c2e265f7cfc652f93406975313a
RAMKUMAR-devops <59164612+RAMKUMAR-devops@users.noreply.github.com>
2021-10-18 05:30:40 -0500

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 sudo yum update -y
 sudo sudo amazon-linux-extras install nginx1 -y
-sudo systemctl start nginx  
-echo "<h1>Welcome to AWS DevOps World!</h1>" | sudo tee /usr/share/nginx/html/index.html
+sudo systemctl start nginx 
+sudo systemctl enable nginx
+echo "<h1>HELLO !!! This is my assignment</h1>" | sudo tee /usr/share/nginx/html/index.html
