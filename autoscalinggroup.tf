95e644daf37ccbf5f69976398fd10e99b388fdef
RAMKUMAR-devops <59164612+RAMKUMAR-devops@users.noreply.github.com>
2021-10-18 05:15:33 -0500

    Update autoscalinggroup.tf

--- autoscalinggroup.tf
+++ autoscalinggroup.tf
@@ -18,16 +18,16 @@ resource "aws_autoscaling_group" "webtierasg" {
 
 resource "aws_autoscaling_attachment" "web_asg_attachment" {
   alb_target_group_arn   = aws_alb_target_group.web_target_group.arn
-  autoscaling_group_name = aws_autoscaling_group.web_asg.id
+  autoscaling_group_name = aws_autoscaling_group.webtierasg.id
 }
 
 
-resource "aws_autoscaling_group" "app_asg" {
-  name                = "app-asg"
+resource "aws_autoscaling_group" "apptierasg" {
+  name                = "apptier-asg"
   max_size            = 2
   min_size            = 1
   desired_capacity    = 2
-  vpc_zone_identifier = [aws_subnet.demo_private_subnet_1.id, aws_subnet.demo_private_subnet_2.id]
+  vpc_zone_identifier = [aws_subnet.my_private_subnet_1.id, aws_subnet.my_private_subnet_2.id]
 
   launch_template {
     id      = aws_launch_template.app_tier_lt.id
@@ -38,5 +38,5 @@ resource "aws_autoscaling_group" "app_asg" {
 
 resource "aws_autoscaling_attachment" "app_asg_attachment" {
   alb_target_group_arn   = aws_alb_target_group.app_target_group.arn
-  autoscaling_group_name = aws_autoscaling_group.app_asg.id
+  autoscaling_group_name = aws_autoscaling_group.apptierasg.id
 }
