50ad8982e98ae0c46c00c59cc657458ab9e5cd01
RAMKUMAR-devops <59164612+RAMKUMAR-devops@users.noreply.github.com>
2021-10-18 08:32:30 -0500

    Update launchtemplate.tf

--- launchtemplate.tf
+++ launchtemplate.tf
@@ -1,6 +1,6 @@
 
-resource "aws_key_pair" "tcs_server_key" {
-  key_name   = "tcs_server_key"
+resource "aws_key_pair" "server_key" {
+  key_name   = "server_key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDbb71xvhhu17PzSHNMGtUeoo1cEiw0HTW370njTBHiq/MRameGBAab75veiZPEV/oemcIg6STzLl9yPNlr6gm4vWaViyNnEGWaH1CPXwhbT//TMsjMVAc0wlyENlLMt6ttZ/FzVYe/if5lW6124/YDQXyq8pbPsBpIXiF+4IW259er0boL6Oy5Aw1xMujZPnxykyozfZ/tNjUOZncsl7YJ+TxX6yDpOcGXbT5put0sAGI39SVw1LrO1ub1Xat05Oc71+SktwSjMQ/L3p2wULEwaZvlO/iATKOenRcwiBp0f4kJduP5ButVTHkgJPI9Qadumrh6qSJ+lRfT3S0heYR1 root@ip-172-31-23-185.ec2.internal"
 }
 
@@ -19,14 +19,14 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
-resource "aws_launch_template" "web_tier_lt" {
+resource "aws_launch_template" "web_tier_template" {
   name = "web-tier-launch-template"
 
   image_id = data.aws_ami.amazon_linux_2.id
 
   instance_type = var.instance_type
 
-  key_name = aws_key_pair.tcs_server_key.id
+  key_name = aws_key_pair.server_key.id
 
   vpc_security_group_ids = ["${aws_security_group.web_sg.id}"]
 
@@ -43,7 +43,7 @@ resource "aws_launch_template" "web_tier_lt" {
 
 }
 
-resource "aws_launch_template" "app_tier_lt" {
+resource "aws_launch_template" "app_tier_template" {
   name = "app-tier-launch-template"
 
   iam_instance_profile {
@@ -54,7 +54,7 @@ resource "aws_launch_template" "app_tier_lt" {
 
   instance_type = var.instance_type
 
-  key_name = aws_key_pair.tcs_server_key.id
+  key_name = aws_key_pair.server_key.id
 
   vpc_security_group_ids = ["${aws_security_group.app_sg.id}"]
 
